9703cd648823c56caebe49a21daffbfe49fae61b
Matheus Daumas <daumasmatheus@gmail.com>
2020-10-26 18:56:47 -0300

    Criação da configuração global para autorização dos endpoints atraves de roles do Identity

---
+++ src/ProjectManager.Core/Configuration/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectManager.Core.Configuration
+{
+    class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiresIn { get; set; }
+        public string Emitter { get; set; }
+        public string ValidIn { get; set; }
+    }
+}
