a01b54fa50d761cdb9b788bd3136209ec95e9f09
Matheus Daumas <daumasmatheus@gmail.com>
2020-10-27 19:52:19 -0300

    Configuração inicial do ocelotswagger

--- src/ProjectManager.Gateway/Startup.cs
+++ src/ProjectManager.Gateway/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,13 +21,25 @@ namespace ProjectManager.Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot(Configuration);
+            services.AddSwaggerForOcelot(Configuration);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/gateway");
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseSwaggerForOcelotUI(opts =>
+            {
+                opts.DownstreamSwaggerEndPointBasePath = "/gateway/swagger/docs";
+                opts.PathToSwaggerGenerator = "/swagger/docs";
+            });
 
             app.UseOcelot().Wait();
         }
