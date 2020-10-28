960069509f3e7273c8f0d233bf8b1a076d61e68d
Matheus Daumas <daumasmatheus@gmail.com>
2020-10-28 18:30:42 -0300

    Criação do microservico da entidade Team

---
+++ src/ProjectManager.Team/Configuration/ApiConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Infrastructure.Data;
+using ProjectManager.Infrastructure.Data.Interfaces;
+using ProjectManager.Team.Services;
+
+namespace ProjectManager.Team.Configuration
+{
+    public static class ApiConfiguration
+    {
+        public static IServiceCollection AddApiConfiguration (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            services.AddTransient<IDatabaseConnectionFactory>(e =>
+            {
+                return new DatabaseConnectionFactory(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<TeamServices>();
+
+            return services;
+        }
+    }
+}
---
+++ src/ProjectManager.Team/Services/TeamServices.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManager.Infrastructure.Data.Interfaces;
+
+namespace ProjectManager.Team.Services
+{
+    public class TeamServices
+    {
+        public readonly IDatabaseConnectionFactory connectionFactory;
+
+        public TeamServices(IDatabaseConnectionFactory ConnectionFactory)
+        {
+            connectionFactory = ConnectionFactory;
+        }
+    }
+}
