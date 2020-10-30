a3e7b15c9ef1341ae937f40cb954acf96d045ee6
Matheus Daumas <daumasmatheus@gmail.com>
2020-10-30 19:40:35 -0300

    Criacao do servico de login

--- src/ProjectManager-client/src/app/components/account/log-in/log-in.component.ts
+++ src/ProjectManager-client/src/app/components/account/log-in/log-in.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar} from "@angular/material/snack-bar";
+
+
+import { User } from '../models/user.model';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-log-in',
@@ -8,9 +13,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService: AccountService,
+              private snackBar: MatSnackBar) { }
 
   loginForm: FormGroup;
+  user: User;
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
@@ -23,5 +30,37 @@ export class LogInComponent implements OnInit {
     return this.loginForm.controls[controlName].hasError(errorName);
   }
 
+  login(){
+    if (this.loginForm.dirty && this.loginForm.valid){
+      this.user = Object.assign({}, this.user, this.loginForm.value);
 
+      this.accountService.login(this.user)
+          .subscribe(
+            response => {this.proccessResult(response.value)},
+            error => {this.proccessError(error)}
+      );
+    }
+  }
+
+  proccessResult = (response: any) => {
+    this.loginForm.reset();
+    
+    this.accountService.localStorage.saveUserData(response);
+
+    this.openSnackBar('Usuário logado com sucesso');
+  }
+
+  proccessError = (fail: any) => {
+    fail.error.errors.Messages.forEach(errorMessage => {
+      this.openSnackBar(errorMessage);
+    });    
+  }
+
+  openSnackBar = (message: string) => {
+    this.snackBar.open(message, 'X', {
+      duration: 15000,
+      horizontalPosition: 'start',
+      verticalPosition: 'top'
+    });
+  }
 }
