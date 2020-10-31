71df0fb6c22cd4e6a20377c6e2ed490c4ff85e46
Matheus Daumas <daumasmatheus@gmail.com>
2020-10-31 19:53:04 -0300

    Definicao da rota inicial da aplicação

--- src/ProjectManager-client/src/app/app-routing.module.ts
+++ src/ProjectManager-client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'account' },
+  { path: '', pathMatch: 'full', redirectTo: 'account/login' },
   {
     path: 'account', 
     loadChildren: () => import('./components/account/account.module').then(m => m.AccountModule)
