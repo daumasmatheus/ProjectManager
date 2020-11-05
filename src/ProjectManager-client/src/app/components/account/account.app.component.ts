c24a85937f19cf2e4c74e8fca8a63b133d1b3e03
Matheus Daumas <daumasmatheus@gmail.com>
2020-11-05 18:53:00 -0300

    Reestilização da tela de login/criação de contas e inclusao dos logotipos

--- src/ProjectManager-client/src/app/components/account/account.app.component.ts
+++ src/ProjectManager-client/src/app/components/account/account.app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-account',
-    templateUrl: './account.component.html'
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css']
 })
 export class AccountComponent { }
