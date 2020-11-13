82fdd720418c5e1d46f5fcc923c2ac9d9be2ead1
Matheus Daumas <daumasmatheus@gmail.com>
2020-11-13 18:25:03 -0300

    Criado o metodo para efetuar o logout do usuário

--- src/ProjectManager-client/src/app/components/dashboard/dashboard.app.component.ts
+++ src/ProjectManager-client/src/app/components/dashboard/dashboard.app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageUtils } from 'src/app/helpers/localstorage';
+import { MessageType } from 'src/app/helpers/message-type.enum';
+import { SnackBarHelper } from 'src/app/helpers/snack-bar.helper';
 
 @Component({
     selector: 'app-dashbord',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent { }
+export class DashboardComponent { 
+    constructor (private snackHelper: SnackBarHelper,
+                 private router: Router) {}
+
+    private localStorageUtils = new LocalStorageUtils();
+
+    logout() {
+        this.localStorageUtils.clearLocalUserData();
+
+        this.snackHelper.showSnackbar("Efetuando logout", MessageType.OkMessage, 3000);
+        setTimeout(() => {
+            this.router.navigate(['/account/login']);
+        }, 3000);
+    }
+
+}
