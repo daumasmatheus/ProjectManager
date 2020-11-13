04be4ea81ff73543b1b6a94132e881c261bd7e57
Matheus Daumas <daumasmatheus@gmail.com>
2020-11-13 18:40:06 -0300

    Exibicao do nome do usuario logado na sidebar e iniciais no avatar

--- src/ProjectManager-client/src/app/components/dashboard/dashboard.app.component.ts
+++ src/ProjectManager-client/src/app/components/dashboard/dashboard.app.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { LocalStorageUtils } from 'src/app/helpers/localstorage';
 import { MessageType } from 'src/app/helpers/message-type.enum';
 import { SnackBarHelper } from 'src/app/helpers/snack-bar.helper';
+import { User } from '../account/models/user.model';
 
 @Component({
     selector: 'app-dashbord',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent { 
+export class DashboardComponent implements OnInit { 
+
     constructor (private snackHelper: SnackBarHelper,
-                 private router: Router) {}
+                 private router: Router) {}    
 
     private localStorageUtils = new LocalStorageUtils();
+
+    userName: string;
+
+    ngOnInit(): void {
+        var loggedUser = this.localStorageUtils.getUser();
+        this.userName = `${loggedUser.firstName} ${loggedUser.lastName}`;
+    }
 
     logout() {
         this.localStorageUtils.clearLocalUserData();
@@ -23,5 +33,4 @@ export class DashboardComponent {
             this.router.navigate(['/account/login']);
         }, 3000);
     }
-
 }
