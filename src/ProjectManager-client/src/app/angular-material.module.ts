d4a572a69892d779afdf17da34fc7f84326ab3b0
Matheus Daumas <daumasmatheus@gmail.com>
2020-11-19 19:04:55 -0300

    Inclusao de novos componentes do angular material

--- src/ProjectManager-client/src/app/angular-material.module.ts
+++ src/ProjectManager-client/src/app/angular-material.module.ts
@@ -9,7 +9,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatRippleModule } from '@angular/material/core';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -33,6 +33,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { CdkStepperModule } from "@angular/cdk/stepper";
+import { MatStepperModule } from "@angular/material/stepper";
 
 
 const materialModules = [
@@ -68,7 +70,10 @@ const materialModules = [
   MatRadioModule,
   MatDatepickerModule,
   MatTooltipModule,
-  MatProgressBarModule
+  MatProgressBarModule,
+  MatNativeDateModule,
+  CdkStepperModule,
+  MatStepperModule
 ];
 
 @NgModule({
