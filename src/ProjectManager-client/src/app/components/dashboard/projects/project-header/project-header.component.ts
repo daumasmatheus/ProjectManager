faad813bb4600555a184b16bf92f665077083989
Matheus Daumas <daumasmatheus@gmail.com>
2020-11-19 19:05:31 -0300

    Criacao do componente header da tela de projetos

---
+++ src/ProjectManager-client/src/app/components/dashboard/projects/project-header/project-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-project-header',
+  templateUrl: './project-header.component.html',
+  styleUrls: ['./project-header.component.css']
+})
+export class ProjectHeaderComponent implements OnInit {
+
+  constructor() { }  
+
+  ngOnInit(): void {
+  }
+}
