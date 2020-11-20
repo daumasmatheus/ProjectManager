aff4260a8c7e71af386e880b1e11184d99e11bfd
Matheus Daumas <daumasmatheus@gmail.com>
2020-11-20 18:40:05 -0300

    Substituido o codigo da tabela pelo componente criado

--- src/ProjectManager-client/src/app/components/dashboard/projects/new-project/new-project.component.ts
+++ src/ProjectManager-client/src/app/components/dashboard/projects/new-project/new-project.component.ts
@@ -1,8 +1,4 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 export interface Person {
   id: number,
@@ -20,65 +16,14 @@ const NAMES: string[] = [
   styleUrls: ['./new-project.component.css']
 })
 export class NewProjectComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['select', 'id', 'name'];
-  dataSource: MatTableDataSource<Person>;
-  selection = new SelectionModel<Person>(true, []);
+  displayedColumns: string[] = ['id', 'name'];
+  people: Person[] = Array.from({length: 100}, (_, k) => createNewPerson(k+1));
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  constructor() { }
 
-  constructor() { 
-    const users = Array.from({length: 100}, (_, k) => createNewPerson(k+1));
+  ngAfterViewInit(): void { }
 
-    this.dataSource = new MatTableDataSource(users);
-  }
-
-  ngAfterViewInit(): void {
-    this.paginator._intl.nextPageLabel = 'Próximo';
-    this.paginator._intl.previousPageLabel = 'Anterior';
-    this.paginator._intl.itemsPerPageLabel = 'Itens por página';
-    this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
-      const start = page * pageSize + 1;
-      const end = (page + 1) * pageSize;
-      return `${start} - ${end} de ${length}`;
-    }
-
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  ngOnInit(): void {
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-
-    return numSelected === numRows;
-  }
-
-  masterToggle(){
-    this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
-    console.log('selected', this.selection.selected);
-  }
-
-  checkboxLabel(row?: Person){
-    if(!row) {
-      return `${this.isAllSelected() ? 'selecionar' : 'remover'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'remover' : 'selecionar'} ${row.id + 1}`;
-  }
-
+  ngOnInit(): void { }
 }
 
 function createNewPerson(id: number): Person {
