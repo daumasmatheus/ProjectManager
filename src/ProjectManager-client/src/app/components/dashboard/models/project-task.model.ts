8748852a0d39a6a1595b085b1a39547bed65aab1
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-02 19:37:58 -0300

    Model de nova tarefa e modificacoes no componente de nova tarefa

---
+++ src/ProjectManager-client/src/app/components/dashboard/models/project-task.model.ts
@@ -0,0 +1,14 @@
+import { Person } from './person.model';
+
+export class ProjectTask {
+    constructor(_id: number) {
+        this.id = _id;
+    }
+
+    id: number;
+    title: string;
+    priority: number;
+    conclusionDate: Date;
+    description: string;
+    attendant: Person;
+}
--- src/ProjectManager-client/src/app/components/dashboard/projects/add-task-dialog/add-task-dialog.component.ts
+++ src/ProjectManager-client/src/app/components/dashboard/projects/add-task-dialog/add-task-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { inject } from '@angular/core/testing';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DateCantBeLowerThanToday, TaskConclusionDateCantBeHigherThanProjectConclusionDate } from 'src/app/helpers/dateValidators.validator';
+import { Person } from '../../models/person.model';
+import { ProjectTask } from '../../models/project-task.model';
 
 @Component({
   selector: 'app-add-task-dialog',
@@ -8,19 +11,58 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./add-task-dialog.component.css']
 })
 export class AddTaskDialogComponent implements OnInit {
-  PEOPLE: string[] = [
-    'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-    'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-  ];
+  attendants: Person[];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-              public dialogRef: MatDialogRef<AddTaskDialogComponent>) { }
+              public dialogRef: MatDialogRef<AddTaskDialogComponent>) { }  
+
+  taskForm: FormGroup;
+  taskData: ProjectTask;
+  selectedAttendant: Person;
+  projectConclusionDate: Date;
 
   ngOnInit(): void {
+    this.projectConclusionDate = this.data?.projectConclusionDate;
+
+    this.taskForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      priority: new FormControl('', [Validators.required]),
+      conclusionDate: new FormControl('', [Validators.required, 
+                                           DateCantBeLowerThanToday, 
+                                           TaskConclusionDateCantBeHigherThanProjectConclusionDate(this.projectConclusionDate)]),
+      description: new FormControl('', [Validators.required]),
+      attendant: new FormControl('', [Validators.required])
+    });
+
+    if (this.data != null && this.data.newTask){
+      this.taskData = new ProjectTask(this.data.newTask.id);
+      this.attendants = this.data.selectedAttendants;      
+    }
+    if (this.data != null && this.data.taskToEdit){
+      this.taskForm.patchValue(this.data.taskToEdit);
+      this.selectedAttendant = this.data.taskToEdit.attendant;      
+
+      this.attendants = this.data.selectedAttendants;
+    }    
   }
+
+  saveTaskData() {
+    this.taskData = Object.assign({}, this.taskData, this.taskForm.value);
+    if (this.taskData.id == null)
+      this.taskData.id = this.data.taskToEdit.id;
+
+    this.dialogRef.close(this.taskData);
+  }  
 
   closeDialog() {
     this.dialogRef.close();
   }
+  
+  compareSelected(option, value) {
+    return option.id == value.id;
+  }
 
+  hasError(controlName: string, errorName: string) {
+    return this.taskForm.controls[controlName].hasError(errorName);
+  }
 }
