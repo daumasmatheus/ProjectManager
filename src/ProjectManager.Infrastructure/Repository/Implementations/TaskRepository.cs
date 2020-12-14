2f849fc5957973910025a08be3e56b8fa0b7d513
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-14 18:13:29 -0300

    Repositorio da entidade Task

---
+++ src/ProjectManager.Infrastructure/Repository/Implementations/TaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProjectManager.Core.Entities;
+using ProjectManager.Infrastructure.Data.DatabaseContext;
+using ProjectManager.Infrastructure.Repository.Abstractions;
+using ProjectManager.Infrastructure.Repository.Interfaces;
+
+namespace ProjectManager.Infrastructure.Repository.Implementations
+{
+    public class TaskRepository : RepositoryBase<Task>, ITaskRepository
+    {
+        public TaskRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) { }
+    }
+}
---
+++ src/ProjectManager.Infrastructure/Repository/Interfaces/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProjectManager.Core.Entities;
+
+namespace ProjectManager.Infrastructure.Repository.Interfaces
+{
+    public interface ITaskRepository : IRepositoryBase<Task>
+    {
+
+    }
+}
