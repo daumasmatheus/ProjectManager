031ddfea95dd948485e8a50fb2a26f0b1e3eb345
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-14 18:28:58 -0300

    Refatoracao para uso da dto na classe de teste

---
+++ src/ProjectManager.Infrastructure.Tests/IntegrationTests/TaskIntegrationTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using ProjectManager.API.DTOs;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace ProjectManager.Infrastructure.Tests.IntegrationTests
+{
+    public class TaskIntegrationTests : IDisposable
+    {
+        private TestServer server;
+        private HttpClient client;
+
+        public TaskIntegrationTests()
+        {
+            var webBuilder = new WebHostBuilder().UseStartup<Startup>();
+            server = new TestServer(webBuilder);
+            client = server.CreateClient();
+        }
+
+        [Fact]
+        public async void Task_ShouldNotFoundEndpoint()
+        {
+            var response = await client.GetAsync("/api/task/blabla");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async void Task_ShoudPostNewTask()
+        {
+            var taskSample = new TaskDto
+            {
+                TaskId = Guid.NewGuid(),
+                Title = "Task Test",
+                StatusId = 1,
+                PriorityId = 1,
+                Description = "Task Test",
+                CreatedDate = DateTime.Now
+            };
+
+            HttpRequestMessage postRequest = new HttpRequestMessage(HttpMethod.Post, "api/task/PostNewTask");
+            postRequest.Content = new StringContent(JsonSerializer.Serialize(taskSample), Encoding.UTF8, "application/json");
+
+            var response = await client.SendAsync(postRequest);
+            response.EnsureSuccessStatusCode();
+
+            var responseStr = await response.Content.ReadAsStringAsync();
+            var responseObj = JsonSerializer.Deserialize<TaskDto>(responseStr);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsType<TaskDto>(responseObj);
+            Assert.NotNull(responseObj);
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
