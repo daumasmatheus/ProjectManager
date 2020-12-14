5b34bce2b8afcb1d6ba9020f7eb3738f12c152d8
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-14 18:08:25 -0300

    Criacao da configuracao do dbcontext

---
+++ src/ProjectManager.API/Configuration/DbContextConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Infrastructure.Data.DatabaseContext;
+
+namespace ProjectManager.API.Configuration
+{
+    public static class DbContextConfiguration
+    {
+        public static IServiceCollection AddMainDbContextConfiguration(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(opts =>
+                opts.UseSqlServer(config.GetConnectionString("DefaultConnection"))
+            );
+
+            return services;
+        }
+    }
+}
