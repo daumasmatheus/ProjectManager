8bf3b5a3414f5eb28fd9dec33854dfd8d6e63624
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-14 18:11:28 -0300

    Criacao e configuracao da extensao da classe de criacao de usuario

--- src/ProjectManager.Core/Extensions/ApplicationUser.cs
+++ src/ProjectManager.Core/Extensions/ApplicationUser.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.Security.Policy;
+using ProjectManager.Core.Entities;
+using System;
 
 namespace ProjectManager.Core.Extensions
 {
     public class ApplicationUser : IdentityUser
     {
+        public Guid PersonId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Provider { get; set; } = "LOCAL";
         public string ExternalUserId { get; set; }
+
+        public Person Person { get; set; }
     }
 }
