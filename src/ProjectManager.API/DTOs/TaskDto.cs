1659a7dc3b5535d456e4e91b259e2f86ad68ff19
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-14 18:26:39 -0300

    Criacao das primeiras Dtos

---
+++ src/ProjectManager.API/DTOs/TaskDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManager.API.DTOs
+{
+    public class TaskDto
+    {
+        public Guid TaskId { get; set; }
+        public Guid? ProjectId { get; set; }
+        public int StatusId { get; set; }
+        public int PriorityId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? ConclusionDate { get; set; }
+        public DateTime? ConcludedDate { get; set; }
+
+        //public Person Person { get; set; }
+        //public Project Project { get; set; }
+        //public Status Status { get; set; }
+        //public Priority Priority { get; set; }
+        //public ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ src/ProjectManager.API/DTOs/UserDto.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManager.API.DTOs
+{
+    public class UserRegisterDto
+    {
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Entre com pelo menos 2 caracteres")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Entre com pelo menos 2 caracteres")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [EmailAddress(ErrorMessage = "Campo {0} com formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [StringLength(100, ErrorMessage = "A senha deve ter entre {2} e {1}", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "As senhas não conferem")]
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class ExternalUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Provider { get; set; }
+        public string ExternalUserId { get; set; }
+    }
+
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [EmailAddress(ErrorMessage = "Campo {0} com formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [StringLength(100, ErrorMessage = "A senha deve ter entre {2} e {1}", MinimumLength = 8)]
+        public string Password { get; set; }
+    }
+
+    public class UserLoginTokenResponseDto
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenDto UserToken { get; set; }
+    }
+
+    public class UserTokenDto
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<UserClaimDto> Claims { get; set; }
+    }
+
+    public class UserClaimDto
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
