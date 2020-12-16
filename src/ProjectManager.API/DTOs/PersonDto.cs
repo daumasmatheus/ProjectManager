da47eb1f0368fbd9bbb59777fc8a1ba15fe4ee5c
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-16 19:14:36 -0300

    Criacao da persondto

---
+++ src/ProjectManager.API/DTOs/PersonDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectManager.API.DTOs
+{
+    public class PersonDto
+    {
+        public Guid PersonId { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public ICollection<TaskDto> Tasks { get; set; }
+        public ICollection<ProjectDto> Projects { get; set; }
+        public ICollection<CommentDto> Comments { get; set; }
+    }
+}
