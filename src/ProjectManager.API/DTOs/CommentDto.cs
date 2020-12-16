46524bbd42d636f75a0532a216e253da7cd4dd87
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-16 19:14:23 -0300

    Criacao da CommentDto

---
+++ src/ProjectManager.API/DTOs/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectManager.API.DTOs
+{
+    public class CommentDto
+    {
+        public Guid CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public PersonDto Person { get; set; }
+        public TaskDto Task { get; set; }
+    }
+}
