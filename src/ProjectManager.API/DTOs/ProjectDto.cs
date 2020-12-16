5d420fff83994a52570d754a8d68f0a858778be1
Matheus Daumas <daumasmatheus@gmail.com>
2020-12-16 19:14:49 -0300

    Criacao da projectDto

---
+++ src/ProjectManager.API/DTOs/ProjectDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectManager.API.DTOs
+{
+    public class ProjectDto
+    {
+        public Guid ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ConclusionDate { get; set; }
+        public DateTime? ConcludedDate { get; set; }
+
+        public List<Task> Tasks { get; set; }
+        public PersonDto Author { get; set; }
+    }
+}
